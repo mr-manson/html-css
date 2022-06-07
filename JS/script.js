ee558cd3705abcdd8409fec8ac0b4c82a7b58863
mr-manson <nikola777jr@gmail.com>
2022-06-07 23:52:30 +0300

    change script.js

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+"use strict"; // строгий режим
+let num = 123;
+let a, b;
+a = 1;
+b = a;
+
+//alert(num + a + b);
+
+num = 10;
+//alert(num);
+
+let x = 13, y = 5;
+//alert(x % y);
+
+let c = 2;
+//alert(y ** c); // возведение в степень
+//alert(2 * 2 ** 2); // степень имеет приоритет
+
+let str = "Aleh";
+alert(str + " " + str.length);
+alert("abcdef".length); // длина строки
+
+
+/*
+комментарий
+*/
